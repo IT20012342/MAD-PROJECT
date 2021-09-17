b453f478b144a40d9e43b0c0cb55bd9216b595ac
IT20012342 <it20012342@gmail.com>
2021-09-17 17:54:37 +0530

    Database connection and Login

---
+++ app/src/main/java/com/example/teacherapp/User.java
@@ -0,0 +1,31 @@
+package com.example.teacherapp;
+
+public class User {
+
+    String email, mobile;
+
+    public User() {
+
+    }
+
+    public User(String email, String mobile) {
+        this.email = email;
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}
