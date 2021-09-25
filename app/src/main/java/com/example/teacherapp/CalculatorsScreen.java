0d5206c83976bab911e3ca46b757bdd7e2deb3e1
AvishkaHashenu <avishka.hashenu@gmail.com>
2021-09-25 23:26:13 +0530

    Merged sheduler

---
+++ app/src/main/java/com/example/teacherapp/CalculatorsScreen.java
@@ -0,0 +1,13 @@
+package com.example.teacherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalculatorsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculators_screen);
+    }
+}
