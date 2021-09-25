32902e5b43f7789fbc3f97ea2729d9003ee17af3
malinya <it20211332@my.sliit.lk>
2021-09-26 01:04:07 +0530

    Updated calculators

--- app/src/main/java/com/example/teacherapp/CalculatorsScreen.java
+++ app/src/main/java/com/example/teacherapp/CalculatorsScreen.java
@@ -1,13 +1,44 @@
 package com.example.teacherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class CalculatorsScreen extends AppCompatActivity {
+    ImageView gpai, vcal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculators_screen);
+
+        gpai = findViewById(R.id.gpaii);
+        vcal = findViewById(R.id.voicecal);
+
+        gpai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CalculatorsScreen.this, GPAcal.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        vcal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CalculatorsScreen.this, VoiceCal.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+
     }
+
+
 }
