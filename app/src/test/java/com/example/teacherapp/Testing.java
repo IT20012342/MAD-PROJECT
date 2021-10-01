32f7e805504c9815580455f9e6787a2c84463924
Tinul Piumika <it20188054@my.sliit.lk>
2021-10-01 22:32:26 +0530

    implement unit test case

--- app/src/test/java/com/example/teacherapp/Testing.java
+++ app/src/test/java/com/example/teacherapp/Testing.java
@@ -1,5 +1,7 @@
 package com.example.teacherapp;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -10,10 +12,19 @@ import java.util.Date;
 public class Testing {
     private EventRecyclerAdapter eventRecyclerAdapter;
 
+    private Attendance attendance;
+
     @BeforeEach
     public void setUp(){
         eventRecyclerAdapter = new EventRecyclerAdapter();
     }
+
+    @BeforeEach
+    public void setup(){
+        attendance = new Attendance();
+    }
+
+
 
     @Test
     public void testConvertStringToDate(){
@@ -26,4 +37,15 @@ public class Testing {
         Date Result = eventRecyclerAdapter.ConvertStringToTime("Thu Jan 01 07:00:00 GMT+05:30 2021");
         assertEquals("07:00",Result);
     }
+
+
+    @Test
+    public void summaryCalculation_isCorrect() {
+        Object ob = null;
+        long result = attendance.calculateSummary(ob,20);
+        Assert.assertEquals(20, result);
+    }
+
+
+
 }
